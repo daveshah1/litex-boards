4db76f71904404f9f49e46145194a7721b620bea
David Shah <dave@ds0.me>
2019-07-03 14:42:10 +0100

    wip
    Signed-off-by: David Shah <dave@ds0.me>

--- litex_boards/community/targets/zcu104.py
+++ litex_boards/community/targets/zcu104.py
@@ -17,7 +17,7 @@ from litedram.phy import usddrphy
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
-    def __init__(self, platform, sys_clk_freq):
+    def __init__(self, platform, sys_clk_freq, with_sdram):
         self.clock_domains.cd_sys = ClockDomain()
         self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
         self.clock_domains.cd_clk200 = ClockDomain()
@@ -29,7 +29,9 @@ class _CRG(Module):
         self.cd_sys4x.clk.attr.add("keep")
 
         self.submodules.pll = pll = USMMCM(speedgrade=-2)
-        self.comb += pll.reset.eq(platform.request("cpu_reset"))
+        cpu_reset = platform.request("cpu_reset")
+        self.comb += pll.reset.eq(cpu_reset)
+        self.comb += platform.request("user_led", 2).eq(pll.locked)
         self.clock_domains.cd_pll4x = ClockDomain(reset_less=True)
         pll.register_clkin(platform.request("clk125"), 125e6)
         pll.create_clkout(self.cd_pll4x, sys_clk_freq*4, buf=None, with_reset=False)
@@ -41,7 +43,7 @@ class _CRG(Module):
                 i_CE=1, i_I=self.cd_pll4x.clk, o_O=self.cd_sys.clk),
             Instance("BUFGCE", name="main_bufgce",
                 i_CE=1, i_I=self.cd_pll4x.clk, o_O=self.cd_sys4x.clk),
-            AsyncResetSynchronizer(self.cd_clk200, ~pll.locked),
+            AsyncResetSynchronizer(self.cd_clk200, ~pll.locked | cpu_reset),
         ]
 
         ic_reset_counter = Signal(max=64, reset=63)
@@ -65,12 +67,21 @@ class _CRG(Module):
                 )
             )
         ]
-        self.specials += [
-            Instance("IDELAYCTRL", p_SIM_DEVICE="ULTRASCALE",
-                     i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset,
-                     o_RDY=ic_rdy),
-            AsyncResetSynchronizer(self.cd_ic, ic_reset)
-        ]
+
+        if with_sdram:
+            self.specials += [
+                Instance("IDELAYCTRL", p_SIM_DEVICE="ULTRASCALE",
+                         i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset,
+                         o_RDY=ic_rdy),
+                AsyncResetSynchronizer(self.cd_ic, ic_reset)
+            ]
+        else:
+            self.comb += [
+                ic_rdy.eq(1)
+            ]
+            self.specials += [
+                AsyncResetSynchronizer(self.cd_ic, ic_reset)
+            ]
 
         # Prevent ARM cores from crashing after loading the bitstream
         self.specials += [
@@ -86,16 +97,29 @@ class BaseSoC(SoCSDRAM):
                          integrated_sram_size=0x8000,
                           **kwargs)
 
-        self.submodules.crg = _CRG(platform, sys_clk_freq)
+        self.submodules.crg = _CRG(platform, sys_clk_freq, not self.integrated_main_ram_size)
+
+        led0 = platform.request("user_led", 0)
+        led1 = platform.request("user_led", 1)
+
+        led_counter_n = Signal(32)
+        self.sync += led_counter_n.eq(led_counter_n + 1)
+        self.comb += led0.eq(led_counter_n[25])
+
+        led_counter = Signal(32)
+        led_counter.reset = 0
+        self.sync += led_counter.eq(led_counter + 1)
+        self.comb += led1.eq(led_counter[26])
 
         # sdram
-        self.submodules.ddrphy = usddrphy.USDDRPHY(platform.request("ddram"), memtype="DDR4", device="ULTRASCALE_PLUS", sys_clk_freq=sys_clk_freq, cmd_latency=1)
-        self.add_csr("ddrphy")
-        self.add_constant("USDDRPHY", None)
-        sdram_module = MT40A512M8(sys_clk_freq, "1:4")
-        self.register_sdram(self.ddrphy,
-                            sdram_module.geom_settings,
-                            sdram_module.timing_settings)
+        if not self.integrated_main_ram_size:
+            self.submodules.ddrphy = usddrphy.USDDRPHY(platform.request("ddram"), memtype="DDR4", device="ULTRASCALE_PLUS", sys_clk_freq=sys_clk_freq, cmd_latency=1)
+            self.add_csr("ddrphy")
+            self.add_constant("USDDRPHY", None)
+            sdram_module = MT40A512M8(sys_clk_freq, "1:4")
+            self.register_sdram(self.ddrphy,
+                                sdram_module.geom_settings,
+                                sdram_module.timing_settings)
 
 
 # Build --------------------------------------------------------------------------------------------
@@ -104,10 +128,14 @@ def main():
     parser = argparse.ArgumentParser(description="LiteX SoC on ZCU104")
     builder_args(parser)
     soc_sdram_args(parser)
+    parser.add_argument("--with-sdram", action="store_true",
+                help="enable DDR4 SODIMM support")
+    parser.add_argument("--with-ethernet", action="store_true",
+                    help="enable Ethernet support")
     args = parser.parse_args()
 
     cls = BaseSoC
-    soc = cls(**soc_sdram_argdict(args))
+    soc = cls(integrated_main_ram_size=0x8000 if not args.with_sdram else None, **soc_sdram_argdict(args))
     builder = Builder(soc, **builder_argdict(args))
     builder.build()
 
